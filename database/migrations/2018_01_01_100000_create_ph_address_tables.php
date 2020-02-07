f9ca3298db5333faf37d3518416a39f3078660c4
Arjay Q. Angeles <aqangeles@gmail.com>
2020-02-07 11:30:42 +0800

    Patch column lengths for database level constraints check.

--- database/migrations/2018_01_01_100000_create_ph_address_tables.php
+++ database/migrations/2018_01_01_100000_create_ph_address_tables.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreatePhAddressTables extends Migration
 {
@@ -15,37 +15,37 @@ class CreatePhAddressTables extends Migration
     {
         Schema::create('regions', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('code');
+            $table->string('code', 9);
             $table->string('name');
-            $table->string('region_id', 10)->index();
+            $table->string('region_id', 2)->index();
         });
 
         Schema::create('provinces', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('code');
+            $table->string('code', 9);
             $table->string('name');
-            $table->string('region_id', 10)->index();
-            $table->string('province_id', 10)->index();
+            $table->string('region_id', 2)->index();
+            $table->string('province_id', 4)->index();
         });
 
         Schema::create('cities', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('code');
+            $table->string('code', 9);
             $table->string('name');
-            $table->string('region_id', 10)->index();
-            $table->string('province_id', 10)->index();
-            $table->string('city_id', 10)->index();
+            $table->string('region_id', 2)->index();
+            $table->string('province_id', 4)->index();
+            $table->string('city_id', 6)->index();
 
             $table->index(['province_id', 'region_id'], 'cities_province_regions');
         });
 
         Schema::create('barangays', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('code');
+            $table->string('code', 9);
             $table->string('name');
-            $table->string('region_id', 10)->index();
-            $table->string('province_id', 10)->index();
-            $table->string('city_id', 10)->index();
+            $table->string('region_id', 2)->index();
+            $table->string('province_id', 4)->index();
+            $table->string('city_id', 6)->index();
 
             $table->index(['province_id', 'region_id'], 'barangay_idx_1');
             $table->index(['city_id', 'province_id', 'region_id'], 'barangay_idx_2');

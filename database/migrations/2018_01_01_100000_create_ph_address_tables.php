dbab3019ff9964d74f14d1dfb2907a5eb245858a
Arjay Angeles <aqangeles@gmail.com>
2024-03-02 10:01:05 +0800

    fix: migration

--- database/migrations/2018_01_01_100000_create_ph_address_tables.php
+++ database/migrations/2018_01_01_100000_create_ph_address_tables.php
@@ -4,59 +4,56 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePhAddressTables extends Migration
+return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    public function up(): void
     {
         Schema::create('regions', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('code', 9)->unique();
             $table->string('name');
             $table->string('region_id', 2)->index();
         });
 
         Schema::create('provinces', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('code', 9)->unique();
             $table->string('name');
             $table->string('region_id', 2)->index();
             $table->string('province_id', 4)->index();
+            $table->index(['region_id', 'province_id'], 'province_idx_1');
         });
 
         Schema::create('cities', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('code', 9)->unique();
             $table->string('name');
             $table->string('region_id', 2)->index();
             $table->string('province_id', 4)->index();
             $table->string('city_id', 6)->index();
 
-            $table->index(['province_id', 'region_id'], 'cities_province_regions');
+            $table->index(['region_id', 'province_id'], 'cities_idx_1');
+            $table->index([ 'region_id', 'province_id', 'city_id'], 'cities_idx_2');
         });
 
         Schema::create('barangays', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('code', 9)->unique();
             $table->string('name');
             $table->string('region_id', 2)->index();
             $table->string('province_id', 4)->index();
             $table->string('city_id', 6)->index();
 
-            $table->index(['province_id', 'region_id'], 'barangay_idx_1');
-            $table->index(['city_id', 'province_id', 'region_id'], 'barangay_idx_2');
+            $table->index(['region_id', 'province_id'], 'barangay_idx_1');
+            $table->index(['region_id', 'province_id', 'city_id'], 'barangay_idx_2');
         });
     }
 
-    public function down()
+    public function down(): void
     {
         Schema::drop('barangays');
         Schema::drop('cities');
         Schema::drop('provinces');
         Schema::drop('regions');
     }
-}
+};
